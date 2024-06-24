40c30c1390628ca6aea2783746f56ef222bb58d6
suraj-savant <surajsavant9990@gmail.com>
2024-06-24 18:25:12 +0530

    USP/NP programs

---
+++ lab/tw5.c
@@ -0,0 +1,23 @@
+
+
+// Write a C/C++ program that creates a zombie and then calls system to execute the ps 
+// command to Verify that the process is zombie
+
+
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(){
+    pid_t pid;
+    if ((pid=fork())<0){
+       perror("fork error");
+    }
+    else if(pid==0){
+        _exit(0);
+    }
+      sleep(4);
+      system("ps -o pid,ppid,state,tty,command");
+      _exit(0);
+}
